1a650f39fc804b0379dab86d3c2cea68e01dc453
Ethan Luan <bingqian.luan@icloud.com>
2018-10-09 13:53:21 +0800

    call xinge sdk

--- src/android/Xinge.java
+++ src/android/Xinge.java
@@ -1,11 +1,16 @@
 package com.github.bqluan.xinge;
 
+import android.util.Log;
+
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushManager;
 
 public class Xinge extends CordovaPlugin {
     public static final String TAG = "Xinge";
@@ -35,7 +40,7 @@ public class Xinge extends CordovaPlugin {
      * @param callbackContext   The callback id used when calling back into JavaScript.
      * @return                  True if the action was valid, false if not.
      */
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
         if ("registerPush".equals(action)) {
             return registerPush(args, callbackContext);
         } else {
@@ -47,8 +52,36 @@ public class Xinge extends CordovaPlugin {
     // LOCAL METHODS
     //--------------------------------------------------------------------------
 
-    private boolean registerPush(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        callbackContext.success("hello " + args.getString(0));
+    private boolean registerPush(JSONArray args, final CallbackContext callbackContext) {
+        // run in background thread
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    XGPushManager.registerPush(
+                        cordova.getActivity(),
+                        args.getString(0), // account
+                        new XGIOperateCallback() {
+
+                            @Override
+                            public void onSuccess(Object data, int flag) {
+                                Log.w(TAG, "+++ . token: " + data);
+                                callbackContext.success(data.toString());
+                            }
+
+                            @Override
+                            public void onFail(Object data, int errCode, String msg) {
+                                Log.w(TAG, "+++ register push fail. token: " + data
+                                        + ", errCode: " + errCode + ", msg:"
+                                        + msg);
+                                callbackContext.error(msg);
+                            }
+                        });
+                } catch (JSONException e) {
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        });
         return true;
     }
 }
